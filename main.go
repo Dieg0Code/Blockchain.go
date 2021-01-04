652271dad0fa5fbb69d3764abf13a127b978e8c4
Diego Obando <diegoobando20@gmail.com>
2021-01-04 09:15:19 -0300

    some coments added

--- main.go
+++ main.go
@@ -11,10 +11,12 @@ import (
 	different pairs
 */
 
+//BlockChain : <-
 type BlockChain struct {
 	blocks []*Block
 }
 
+//Block : basic struct
 type Block struct {
 	Hash     []byte //represent the hash of this block
 	Data     []byte //represent the data inside this block
@@ -22,28 +24,33 @@ type Block struct {
 	// each block inside a blockchain references the last block that was created inside the blockchain
 }
 
+//DeriveHash : Generate hash
 func (b *Block) DeriveHash() {
 	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
 	hash := sha256.Sum256(info)
 	b.Hash = hash[:]
 }
 
+// CreateBlock : Create a block
 func CreateBlock(data string, prevHash []byte) *Block {
 	block := &Block{[]byte{}, []byte(data), prevHash}
 	block.DeriveHash()
 	return block
 }
 
+// AddBlock : Add a new block to the Blockchain
 func (chain *BlockChain) AddBlock(data string) {
 	prevBlock := chain.blocks[len(chain.blocks)-1]
 	new := CreateBlock(data, prevBlock.Hash)
 	chain.blocks = append(chain.blocks, new)
 }
 
+// Genesis : Genesis block
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
+//InitBlockChain : <-
 func InitBlockChain() *BlockChain {
 	return &BlockChain{[]*Block{Genesis()}}
 }
@@ -52,7 +59,7 @@ func main() {
 	chain := InitBlockChain()
 
 	chain.AddBlock("First Block after Genesis")
-	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("SeconD Block after Genesis")
 	chain.AddBlock("Third Block after Genesis")
 
 	for _, block := range chain.blocks {
