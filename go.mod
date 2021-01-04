56dd13a07e439b2dc56e8e3151352fc433c21711
Diego Obando <diegoobando20@gmail.com>
2021-01-04 01:29:15 -0300

    basic blockchain structure

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Dieg0Code/Blockchain.go
+
+go 1.15
+
+require rsc.io/quote v1.5.2
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
+
+/*
+	A Blockchain is essentially a public database that is distributed accross multiple
+	different pairs
+*/
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+type Block struct {
+	Hash     []byte //represent the hash of this block
+	Data     []byte //represent the data inside this block
+	PrevHash []byte //represent the last block hash, this allow us to link the block together
+	// each block inside a blockchain references the last block that was created inside the blockchain
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
+func main() {
+	chain := InitBlockChain()
+
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
+}
